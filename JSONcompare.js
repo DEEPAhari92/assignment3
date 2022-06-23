bb99a815b14256ae010716988cd868681f51a23b
DEEPA JEYANDRAN <107227126+DEEPAhari92@users.noreply.github.com>
2022-06-23 12:55:34 +0530

    Add files via upload

---
+++ JSONcompare.js
@@ -0,0 +1,15 @@
+const object1 = {
+    name: 'ABC',
+    address: 'India'
+  };
+      
+  const object2 = {
+    address: 'India',
+    name: 'ABC'
+  };
+_.isEqual(object1, object2);
+
+var object = { 'a': 1 };
+var other = { 'a': 1 };
+ 
+isEqual(object, other);
---
+++ country flag.js
@@ -0,0 +1,10 @@
+var request = new XMLHttpRequest();
+request.open('Get','https://raw.githubusercontent.com/rvsp/restcountries-json-data/master/res-countries.json');
+request.send();
+request.onload = function(){
+    var countryData = JSON.parse(this.responseText);
+    console.log(countryData);
+    for(let country of countryData){
+        console.log(country.flags);
+    }
+}
